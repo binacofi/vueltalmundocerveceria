bf3fa82f39ead8a93d806591a3ec32755e21d9b6
rootspyro <root.spyro@gmail.com>
2024-04-09 01:40:01 -0400

    Feat: Data input, list of beers and pictures

---
+++ src/data.js
@@ -0,0 +1,37 @@
+let beers = [
+  {
+
+    id: "ipa",
+    name: "IPA",
+    description: "Indian Pale Ale, una cerveza de sabores intensos, amarga, con una gran cantidad de lúpulo.",
+    abv: 5.7,
+    price: 3.5,
+    img: "/ipa.jpeg"
+  },
+  {
+    id: "red_irish_ale",
+    name: "Red Irish Ale",
+    description: "Cerveza de origen irlandes, alta fermentación, sabor malteado a caramelo, notas frutales y no suele llevar mucho lúpulo.",
+    abv: 5,
+    price: 3.5,
+    img: "/red_irish_ale.jpeg"
+  },
+  {
+    id: "pilsner",
+    name: "Pilsner",
+    description: "La lager mas fámosa a nivel mundial, una cerveza refrescante, ligera, con aroma a lúpulo y su característico color dorado.",
+    abv: 4.5,
+    price: 3.5,
+    img: "/pilsner.jpeg"
+  },
+  {
+    id: "kolsch",
+    name: "Kölsch",
+    description: "Cerveza ALE de origen alemán, delicada y balanceada, con un caracter sutil a lúpulo y afrutado.",
+    abv: 4.7,
+    price: 3.5,
+    img: "/kolsch.jpeg"
+  },
+]
+
+export default beers

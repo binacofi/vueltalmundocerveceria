d8914cc864d93db41609666747d0cb8d1bf5f6ac
rootspyro <root.spyro@gmail.com>
2024-04-09 14:58:50 -0400

    Feat: Add Car Logic to rewrite products

--- src/carStore.js
+++ src/carStore.js
@@ -2,7 +2,28 @@ import {atom} from "nanostores"
 
 export const car = atom([]);
 
-export function addToCar(beer) {
-  car.set([...car.get(), beer]);
+export function addToCar(newBeer) {
+
+  let copy = car.get()
+  let found = false
+
+  car.set([])
+
+  copy.map(beer => {
+
+    if (beer.id == newBeer.id) {
+
+      beer.quantity
+      found = true
+    }
+
+  })
+
+  if (!found) {
+    copy.push(newBeer)
+  }
+
+  car.set(copy)
   console.log("car: ", car.get())
+ 
 }

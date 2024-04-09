bfa55139a060e6be0ff4af1bcac76fe39752ccc3
rootspyro <root.spyro@gmail.com>
2024-04-09 14:23:09 -0400

    Feat: Add rounded to boxes and buttons

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -3,9 +3,9 @@ export default function ProductCard({product}) {
   return(
     <div className="w-full flex flex-col shadow-lg">
       <div>
-        <img alt="cerveza" class="w-full" src={product?.img} />
+        <img alt="cerveza" className="w-full rounded-t-sm" src={product?.img} />
       </div>
-      <div className="bg-amber-500 text-black p-4 flex flex-col gap-1">
+      <div className="bg-amber-500 text-black p-4 flex flex-col gap-1 rounded-b-sm">
         <div className="flex justify-between">
           <span className="font-medium text-xl">{product?.name}</span> 
           <span className="font-medium text-xl">{product?.abv}%</span> 

94cbc06cc54a4fb152c9f20a1442ca5d08454657
rootspyro <root.spyro@gmail.com>
2024-04-10 15:09:42 -0400

    Feat: Add tailwind anitmations

--- src/components/alert.jsx
+++ src/components/alert.jsx
@@ -3,7 +3,7 @@ export default function Alert({showAlert, product}) {
   return(
     <div className="fixed top-3 left-0 w-full">
       <div className="w-full py-3 px-10 flex justify-end">
-        <div className={`w-full max-w-sm bg-amber-500 p-3 rounded-sm shadow-lg ${showAlert ? "" : "hidden"}`}>
+        <div className={`w-full max-w-sm bg-amber-500 p-3 rounded-sm shadow-lg animate-fade-left duration-100 ${showAlert ? "" : "hidden"}`}>
           <p>Agregado al carrito</p>
           <p className="text-sm font-light">{product?.name}</p>
         </div>

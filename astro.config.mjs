004af6bbfbb1b770c3c0f2aa2a4880c5bec4b667
rootspyro <root.spyro@gmail.com>
2024-04-11 11:25:23 -0400

    Fix: Add astrojs/partytown package to fix build bug when the google analytics tag is added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown'
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
+  integrations: [
+    tailwind(), 
+    preact(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"]
+      }
+    })
+  ]
 });
